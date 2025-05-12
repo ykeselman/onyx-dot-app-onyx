551a05aef06ffac88e1d1ee8c0b9b6419ea8db87
Evan Lohn <evan@danswer.ai>
2025-05-12 14:20:18 -0700

    light worker discovers beat task (#4694)
    * light worker discovers beat task
    
    * v2: put in right place

--- backend/ee/onyx/background/celery/apps/primary.py
+++ backend/ee/onyx/background/celery/apps/primary.py
@@ -134,5 +134,6 @@ celery_app.autodiscover_tasks(
     [
         "ee.onyx.background.celery.tasks.doc_permission_syncing",
         "ee.onyx.background.celery.tasks.external_group_syncing",
+        "ee.onyx.background.celery.tasks.cloud",
     ]
 )
